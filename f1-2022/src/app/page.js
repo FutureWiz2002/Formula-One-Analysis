fdd1cb91e1d68a0f195a7cc3f2b3b35872b14cab
Jabir Mustahid <jabirm2002@gmail.com>
2023-10-10 16:07:19 -0400

    Retry

--- f1-2022/src/app/page.js
+++ f1-2022/src/app/page.js
@@ -1,9 +1,12 @@
-import Chart from 'chart.js/auto';
-// import Constructors from "../../images/Constructors.svg"
-import { ReactComponent as Logo } from "./images/";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 export default function Home() {
+  
+
   return (
   <div>
     <div className='slate-100 rounded-xl p-8 m-5 dark:bg-slate-800'>Welcome to the World of F1 Data Analysis</div>
@@ -11,13 +14,12 @@ export default function Home() {
       <h1>Basic Overview</h1>
     </div>
     <div className='slate-100 rounded-xl p-8 m-5 dark:bg-slate-800'>
-      <h1>Team Analysis</h1>
-      {/* <img src={Logo} width={500} height={500} /> */}
-      <Logo />
+      <h1>Constructors Analysis</h1>
+      {/* <canvas id="myChart"></canvas> */}
     </div>
     <div className='slate-100 rounded-xl p-8 m-5 dark:bg-slate-800'>
       <h1>Driver Analysis</h1>
-      <div><canvas id="acquisitions"></canvas></div>
+      {/* <div><canvas id="acquisitions"></canvas></div> */}
     </div>
   </div>
   )
