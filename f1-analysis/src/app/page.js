d09df4429188cf06edf75ffd8ff754fb58341593
Jabir Mustahid <jabirm2002@gmail.com>
2024-01-16 21:14:04 -0500

    added select option

--- f1-analysis/src/app/page.js
+++ f1-analysis/src/app/page.js
@@ -3,7 +3,30 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <p>Hello world</p>
+      <p>A Comprehensive F1 analysis for 2023</p>
+      <select>
+         <option value="Max Verstappen">Max Verstappen</option>
+         <option value="Sergio Perez">Sergio</option>
+         <option value="Charles Leclerc">Charles Leclerc</option>
+         <option value="Carlos Sainz">Max Verstappen</option>
+         <option value="Goerge Russell">Max Verstappen</option>
+         <option value="Lewis Hamilton">Max Verstappen</option>
+         <option value="Esteban Ocon">Max Verstappen</option>
+         <option value="Pierre Gasly">Max Verstappen</option>
+         <option value="Lando Norris">Max Verstappen</option>
+         <option value="Oscar Piastri">Max Verstappen</option>
+         <option value="Nico Hulkenberg">Max Verstappen</option>
+         <option value="Kevin Magnussen">Max Verstappen</option>
+         <option value="Yuki Tsunoda">Max Verstappen</option>
+         <option value="Daniel Riciardo">Max Verstappen</option>
+         <option value="Valterri Bottas">Max Verstappen</option>
+         <option value="Zhou Guanyu">Max Verstappen</option>
+         <option value="Fernando Alonso">Max Verstappen</option>
+         <option value="Lance Stroll">Max Verstappen</option>
+         <option value="Logan Sergeant">Max Verstappen</option>
+         <option value="Alex Albon">Max Verstappen</option>
+
+      </select>
     </div>
   )
 }
