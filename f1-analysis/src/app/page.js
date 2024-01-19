97143c128a2a64e75d8bcf040dcf5df6e6e1737b
Jabir Mustahid <jabirm2002@gmail.com>
2024-01-18 20:43:26 -0500

    Added template for datasets
    Yet to add the dataset from python

--- f1-analysis/src/app/page.js
+++ f1-analysis/src/app/page.js
@@ -5,13 +5,103 @@ import Chart from 'chart.js/auto';
 import { useState } from 'react'
 import { Line } from 'react-chartjs-2';
 
+const dataSet = {
+  labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+  datasets: [
+    {
+      label: 'Max Verstappen',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Sergio Perez',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Charles Leclerc',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Carlos Sainz',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'George Rusell',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Lewis Hamilton',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Esteban Ocon',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Pierre Gasly',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Lando Norris',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Oscar Piastri',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Nico Hulkenburg',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Kevin Magnussen',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Yuki Tsunoda',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Daniel Riciardo',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Valterri Bottas',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Zhou Guanyu',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },{
+      label: 'Fernanado Alonso',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },{
+      label: 'Lance Stroll',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },{
+      label: 'Logan Sergeant',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+    {
+      label: 'Alex Albon',
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    },
+  ],
+};
+
 
 export default function Home() {
+  const [driverData, setDriverData] = useState('');
+  
+  const changeDriverData = (e) => {
+    setDriverData(e.target.value)
+    console.log(driverData)
+  }
+
 
   return (
     <div>
       <p>A Comprehensive F1 analysis for 2023</p>
-      <select>
+      <select value={driverData} onChange={changeDriverData}>
          <option value="Max Verstappen">Max Verstappen</option>
          <option value="Sergio Perez">Sergio</option>
          <option value="Charles Leclerc">Charles Leclerc</option>
@@ -33,24 +123,12 @@ export default function Home() {
          <option value="Logan Sergeant">Logan Sergeant</option>
          <option value="Alex Albon">Alex Albon</option>
       </select>
-      <Line
-        datasetIdKey='id'
-        data={{
-          labels: ['Jun', 'Jul', 'Aug'],
-          datasets: [
-            {
-              id: 1,
-              label: '',
-              data: [5, 6, 7],
-            },
-            {
-              id: 2,
-              label: '',
-              data: [3, 2, 1],
-            },
-          ],
-  }}
-/>
+      <div className='h-90 w-90'>
+        <Line
+          // datasetIdKey='id'
+          data={dataSet}    />
+      </div>
+      <p>{driverData}</p>
     </div>
   )
 }
