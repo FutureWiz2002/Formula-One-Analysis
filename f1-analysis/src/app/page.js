a9474e1a46c7523dcad5c178096ad895d2029de6
Jabir Mustahid <jabirm2002@gmail.com>
2024-01-21 13:09:15 -0500

    Bug fixed
    Selected driver correctly gives the chart

--- f1-analysis/src/app/page.js
+++ f1-analysis/src/app/page.js
@@ -11,7 +11,7 @@ const allDriverData = {
     'Sergio Perez':[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
     'Charles Leclerc': [26, 19, 26, 8, 18, 0, 12, 0, 10, 12, 25, 0, 8, 8, 15, 18, 18, 15, 15, 8, 12, 18],
     'Carlos Sainz': [18, 15, 0, 0, 15, 12, 18, 0, 19, 25, 0, 11, 12, 15, 4, 12, 15, 0, 0, 10, 15, 12],
-    'George Rusell': [12, 10, 15, 12, 10, 15, 10, 15, 12, 0, 12, 15, 15, 12, 18, 15, 0, 4, 11, 13, 26, 10],
+    'George Russell': [12, 10, 15, 12, 10, 15, 10, 15, 12, 0, 12, 15, 15, 12, 18, 15, 0, 4, 11, 13, 26, 10],
     'Lewis Hamilton': [15, 1, 12, 0, 8, 10, 4, 12, 15, 16, 15, 18, 19, 0, 12, 10, 2, 10, 18, 18, 18, 0],
     'Esteban Ocon': [6, 8, 6, 0, 4, 6, 0, 1, 8, 0, 10, 4, 2, 6, 2, 0, 0, 12, 0, 4, 4, 6],
     'Pierre Gasly': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
@@ -27,13 +27,14 @@ const allDriverData = {
     'Lance Stroll': [0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 8, 0, 0, 0, 1, 4],
     'Logan Sergeant': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
     'Alex Albon': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    '':[]
  
 }
 
 // This function renders the home page
 
 export default function Home() {
-  const [driver, setDriver] = useState('Max Verstappen');
+  const [driver, setDriver] = useState(" ");
   const [chartData, setChartData] = useState({
     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
     datasets: [
@@ -45,51 +46,52 @@ export default function Home() {
   })
   
   const changeDriver = (e) => {
-    setDriver(e.target.value)
+    const selectedDriver = e.target.value
+    setDriver(selectedDriver)
     console.log(driver)
     setChartData({
       labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
       datasets: [
         {
-          label:driver,
-          data:allDriverData[driver]
+          label:selectedDriver,
+          data:allDriverData[selectedDriver]
         }
       ]
     })
   }
 
 
+
   return (
     <div>
       <p>A Comprehensive F1 analysis for 2023</p>
+      <p>{driver}</p>
       <select value={driver} onChange={changeDriver}>
-         <option value="Max Verstappen">Max Verstappen</option>
-         <option value="Sergio Perez">Sergio</option>
-         <option value="Charles Leclerc">Charles Leclerc</option>
-         <option value="Carlos Sainz">Carlos Sainz</option>
-         <option value="George Russell">George Russell</option>
-         <option value="Lewis Hamilton">Lewis Hamilton</option>
-         <option value="Esteban Ocon">Esteban Ocon</option>
-         <option value="Pierre Gasly">Pierre Gasly</option>
-         <option value="Lando Norris">Lando Norris</option>
-         <option value="Oscar Piastri">Oscar Piastri</option>
-         <option value="Nico Hulkenberg">Nico Hulkenberg</option>
-         <option value="Kevin Magnussen">Kevin Magnussen</option>
-         <option value="Yuki Tsunoda">Yuki Tsunoda</option>
-         <option value="Daniel Riciardo">Daniel Riciardo</option>
-         <option value="Valterri Bottas">Valterri Bottas</option>
-         <option value="Zhou Guanyu">Zhou Guanyu</option>
-         <option value="Fernando Alonso">Fernando Alonso</option>
-         <option value="Lance Stroll">Lance Stroll</option>
-         <option value="Logan Sergeant">Logan Sergeant</option>
-         <option value="Alex Albon">Alex Albon</option>
+        <option value=" "></option>
+        <option value="Max Verstappen">Max Verstappen</option>
+        <option value="Sergio Perez">Sergio</option>
+        <option value="Charles Leclerc">Charles Leclerc</option>
+        <option value="Carlos Sainz">Carlos Sainz</option>
+        <option value="George Russell">George Russell</option>
+        <option value="Lewis Hamilton">Lewis Hamilton</option>
+        <option value="Esteban Ocon">Esteban Ocon</option>
+        <option value="Pierre Gasly">Pierre Gasly</option>
+        <option value="Lando Norris">Lando Norris</option>
+        <option value="Oscar Piastri">Oscar Piastri</option>
+        <option value="Nico Hulkenberg">Nico Hulkenberg</option>
+        <option value="Kevin Magnussen">Kevin Magnussen</option>
+        <option value="Yuki Tsunoda">Yuki Tsunoda</option>
+        <option value="Daniel Riciardo">Daniel Riciardo</option>
+        <option value="Valterri Bottas">Valterri Bottas</option>
+        <option value="Zhou Guanyu">Zhou Guanyu</option>
+        <option value="Fernando Alonso">Fernando Alonso</option>
+        <option value="Lance Stroll">Lance Stroll</option>
+        <option value="Logan Sergeant">Logan Sergeant</option>
+        <option value="Alex Albon">Alex Albon</option>
       </select>
       <div className='h-90 w-90'>
-        <Line 
-          data = {chartData}   
-        />
+        <Line data={chartData} />
       </div>
-      <p>{driver}</p>
     </div>
   )
 }
