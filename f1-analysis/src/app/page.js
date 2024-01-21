0f221941a4217df3a27daba4a3fba44c585f5117
Jabir Mustahid <jabirm2002@gmail.com>
2024-01-21 08:08:46 -0500

    Major Update - Almost done
    Updated and restrucutred. Currently displays data correctly.
    One issue - graph does not update according to the state

--- f1-analysis/src/app/page.js
+++ f1-analysis/src/app/page.js
@@ -5,103 +5,64 @@ import Chart from 'chart.js/auto';
 import { useState } from 'react'
 import { Line } from 'react-chartjs-2';
 
-const dataSet = {
-  labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
-  datasets: [
-    {
-      label: 'Max Verstappen',
-      data: [0, 25, 0, 26, 26, 25, 15, 25, 25, 6, 19, 25, 25, 26, 26, 25, 6, 25, 25, 25, 8, 25]
-    },
-    {
-      label: 'Sergio Perez',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },
-    {
-      label: 'Charles Leclerc',
-      data:  [26, 19, 26, 8, 18, 0, 12, 0, 10, 12, 25, 0, 8, 8, 15, 18, 18, 15, 15, 8, 12, 18]
-    },
-    {
-      label: 'Carlos Sainz',
-      data: [18, 15, 0, 0, 15, 12, 18, 0, 19, 25, 0, 11, 12, 15, 4, 12, 15, 0, 0, 10, 15, 12]
-    },
-    {
-      label: 'George Rusell',
-      data: [12, 10, 15, 12, 10, 15, 10, 15, 12, 0, 12, 15, 15, 12, 18, 15, 0, 4, 11, 13, 26, 10]
-    },
-    {
-      label: 'Lewis Hamilton',
-      data: [15, 1, 12, 0, 8, 10, 4, 12, 15, 16, 15, 18, 19, 0, 12, 10, 2, 10, 18, 18, 18, 0] 
-    },
-    {
-      label: 'Esteban Ocon',
-      data: [6, 8, 6, 0, 4, 6, 0, 1, 8, 0, 10, 4, 2, 6, 2, 0, 0, 12, 0, 4, 4, 6]
-    },
-    {
-      label: 'Pierre Gasly',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },
-    {
-      label: 'Lando Norris',
-      data: [0, 6, 10, 15, 0, 4, 9, 2, 0, 8, 6, 6, 6, 0, 6, 6, 12, 1, 8, 2, 0, 9]
-    },
-    {
-      label: 'Oscar Piastri',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },
-    {
-      label: 'Nico Hulkenburg',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },
-    {
-      label: 'Kevin Magnussen',
-      data:  [10, 2, 0, 2, 0, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0]
-    },
-    {
-      label: 'Yuki Tsunoda',
-      data: [4, 0, 0, 6, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-    },
-    {
-      label: 'Daniel Riciardo',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },
-    {
-      label: 'Valterri Bottas',
-      data: [8, 0, 4, 10, 6, 8, 2, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0]
-    },
-    {
-      label: 'Zhou Guanyu',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },{
-      label: 'Fernanado Alonso',
-      data: [2, 0, 0, 0, 0, 2, 6, 6, 2, 10, 1, 8, 4, 10, 8, 0, 0, 6, 6, 0, 10, 0]
-    },{
-      label: 'Lance Stroll',
-      data: [0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 8, 0, 0, 0, 1, 4]
-    },{
-      label: 'Logan Sergeant',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },
-    {
-      label: 'Alex Albon',
-      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
-    },
-  ],
-};
+const allDriverData = {
+  // labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Max Verstappen':[0, 25, 0, 26, 26, 25, 15, 25, 25, 6, 19, 25, 25, 26, 26, 25, 6, 25, 25, 25, 8, 25],
+    'Sergio Perez':[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Charles Leclerc': [26, 19, 26, 8, 18, 0, 12, 0, 10, 12, 25, 0, 8, 8, 15, 18, 18, 15, 15, 8, 12, 18],
+    'Carlos Sainz': [18, 15, 0, 0, 15, 12, 18, 0, 19, 25, 0, 11, 12, 15, 4, 12, 15, 0, 0, 10, 15, 12],
+    'George Rusell': [12, 10, 15, 12, 10, 15, 10, 15, 12, 0, 12, 15, 15, 12, 18, 15, 0, 4, 11, 13, 26, 10],
+    'Lewis Hamilton': [15, 1, 12, 0, 8, 10, 4, 12, 15, 16, 15, 18, 19, 0, 12, 10, 2, 10, 18, 18, 18, 0],
+    'Esteban Ocon': [6, 8, 6, 0, 4, 6, 0, 1, 8, 0, 10, 4, 2, 6, 2, 0, 0, 12, 0, 4, 4, 6],
+    'Pierre Gasly': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Lando Norris': [0, 6, 10, 15, 0, 4, 9, 2, 0, 8, 6, 6, 6, 0, 6, 6, 12, 1, 8, 2, 0, 9],
+    'Oscar Piastri': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Nico Hulkenburg': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Kevin Magnussen':  [10, 2, 0, 2, 0, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+    'Yuki Tsunoda': [4, 0, 0, 6, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    'Daniel Riciardo': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Valterri Bottas': [8, 0, 4, 10, 6, 8, 2, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0],
+    'Zhou Guanyu': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Fernanado Alonso': [2, 0, 0, 0, 0, 2, 6, 6, 2, 10, 1, 8, 4, 10, 8, 0, 0, 6, 6, 0, 10, 0],
+    'Lance Stroll': [0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 8, 0, 0, 0, 1, 4],
+    'Logan Sergeant': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Alex Albon': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+ 
+}
 
+// This function renders the home page
 
 export default function Home() {
-  const [driverData, setDriverData] = useState('');
+  const [driver, setDriver] = useState('Max Verstappen');
+  const [chartData, setChartData] = useState({
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+    datasets: [
+      {
+        label: driver,
+        data: allDriverData[driver]
+      }
+    ]
+  })
   
-  const changeDriverData = (e) => {
-    setDriverData(e.target.value)
-    console.log(driverData)
+  const changeDriver = (e) => {
+    setDriver(e.target.value)
+    console.log(driver)
+    setChartData({
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22],
+      datasets: [
+        {
+          label:driver,
+          data:allDriverData[driver]
+        }
+      ]
+    })
   }
 
 
   return (
     <div>
       <p>A Comprehensive F1 analysis for 2023</p>
-      <select value={driverData} onChange={changeDriverData}>
+      <select value={driver} onChange={changeDriver}>
          <option value="Max Verstappen">Max Verstappen</option>
          <option value="Sergio Perez">Sergio</option>
          <option value="Charles Leclerc">Charles Leclerc</option>
@@ -125,10 +86,11 @@ export default function Home() {
       </select>
       <div className='h-90 w-90'>
         <Line
-          // datasetIdKey='id'
-          data={dataSet}    />
+          
+          data = {chartData}   
+        />
       </div>
-      <p>{driverData}</p>
+      <p>{driver}</p>
     </div>
   )
 }
