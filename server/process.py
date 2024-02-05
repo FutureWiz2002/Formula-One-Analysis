5491a73d719413dd80fd3d906632fa8ccead4b32
Jabir Mustahid <jabirm2002@gmail.com>
2024-02-05 18:48:42 -0500

    Added and parsed 2023 data, testing required

--- server/process.py
+++ server/process.py
@@ -1,8 +1,25 @@
 import pandas as pd
+import json
 
-dataframe = pd.read_csv("F1_2023.csv", encoding='latin-1')
+df = pd.read_csv("F1_2023.csv", encoding='latin-1')
+driverlist = df['Driver']
+print(driverlist)
 
-print(dataframe)
+def driverProfile(driverName):
+    desired_driver_name = driverName
+    result_list = df[df['Driver'].eq(desired_driver_name) & df['Driver'].notna()].values.flatten().tolist()
+    result_list.pop(0)
+    result_list.pop(0)
+    return result_list
 
-def driverProfile():
-    for i in dataframe.
+all_data = {}
+
+for i in driverlist:
+    driverdata = driverProfile(i)
+    all_data[i] = driverdata
+
+for i, j in all_data.items():
+    print(i, j)
+    print("\n")
+
+print(json.dumps(all_data))
